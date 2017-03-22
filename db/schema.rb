9ec924cf66fe02b7eb9c56a7b6097625168da47a
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2017-03-22 20:05:38 +0700

    Create table productTag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322123534) do
+ActiveRecord::Schema.define(version: 20170322130449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20170322123534) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+  end
+
+  create_table "products_tags", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "tag_id"
+    t.index ["product_id"], name: "index_products_tags_on_product_id", using: :btree
+    t.index ["tag_id"], name: "index_products_tags_on_tag_id", using: :btree
   end
 
   create_table "tags", force: :cascade do |t|
